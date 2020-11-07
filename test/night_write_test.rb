b7ea494ccc61f337bac9e1d42b904f0206e79a60
ninesky00 <jiangzho@gmail.com>
2020-11-07 17:55:47 -0600

    Add test for write method

---
+++ test/night_write_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/night_write'
+
+class NightWriteTest < MiniTest::Test
+  def setup
+    @night_write = NightWrite.new
+    @text = ["0.0.0.0.0.", "00.00.0..0", "....0.0.0."]
+    @file = "test_file.txt"
+  end
+
+  def test_attributes_are_instantiated
+    assert_instance_of NightWrite, @night_write
+  end
+
+  def test_can_write_to_file
+    @night_write.write(@text, @file)
+    actual = File.open(@file, "r") do |file|
+      file.read()
+    end
+    expected = "0.0.0.0.0.\n00.00.0..0\n....0.0.0.\n"
+    assert_equal expected, actual
+  end
+end
