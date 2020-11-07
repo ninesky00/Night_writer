a12142a35f001a885ddcdbf891b0abfadd88eb8f
ninesky00 <jiangzho@gmail.com>
2020-11-07 15:50:00 -0600

    Add test for translate method

--- test/to_braille_test.rb
+++ test/to_braille_test.rb
@@ -5,11 +5,15 @@ require './lib/to_braille'
 class ToBrailleTest < MiniTest::Test
   def setup
     @translate = ToBraille.new
+    @string_text = "hello"
   end
 
   def test_attributes_are_instantiated
     assert_instance_of ToBraille, @translate
   end
 
-  
+  def test_can_translate_english_to_braille
+    expected = ["0.", "00", ".."], ["0.", ".0", ".."], ["0.", "0.", "0."], ["0.", "0.", "0."], ["0.", ".0", "0."]
+    assert_equal expected, @translate.translate(@string_text)
+  end
 end
