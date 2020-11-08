6c06de80ce5e7a1b098270217ab1ae2529de7a24
ninesky00 <jiangzho@gmail.com>
2020-11-08 15:36:19 -0600

    Add long string to test

--- test/to_braille_test.rb
+++ test/to_braille_test.rb
@@ -6,6 +6,7 @@ class ToBrailleTest < MiniTest::Test
   def setup
     @translate = ToBraille.new
     @string_text = "hello"
+    @long_string = "hello, this is a long string test so that the test knows to break when lines are too long"
   end
 
   def test_attributes_are_instantiated
@@ -23,9 +24,10 @@ class ToBrailleTest < MiniTest::Test
   end
 
   def test_can_break_new_line_when_output_is_too_long
+    braille_array = @translate.covert_to_write(@long_string)
     expected = "0.0.0.0.0......00..0.0...0.0..0...0.0.0000...0.00..00000...00..0.0...00....00.0\n" +
     "00.00.0..00...00000.0...0.0.......0..0.000..0.00000..000..00.00.00..0..0..0000.\n" +
     "....0.0.0.....0.....0.....0.......0.0.0.....0.0.0...0.....0...0.0...0.0...0...."
-    assert_equal expected, @translate.limit_to_80_characters
+    assert_equal expected, @translate.limit_to_80_characters(braille_array)
   end
 end
