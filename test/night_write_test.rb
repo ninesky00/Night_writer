30ca8f07cf54fa2c07d4913cf9bbccc26398a31f
ninesky00 <jiangzho@gmail.com>
2020-11-08 17:16:11 -0600

    Move 80 character writing test to night_write_test

--- test/night_write_test.rb
+++ test/night_write_test.rb
@@ -4,9 +4,11 @@ require './lib/night_write'
 
 class NightWriteTest < MiniTest::Test
   def setup
-    @night_write = NightWrite.new
-    @text = "0.0.0.0.0.\n00.00.0..0\n....0.0.0.\n"
     @file = "test_file.txt"
+    @night_write = NightWrite.new(@file)
+    @text = "0.0.0.0.0.\n00.00.0..0\n....0.0.0.\n"
+    @string_text = "hello"
+    @long_string = "this is a forty-one character string test"
   end
 
   def test_attributes_are_instantiated
@@ -20,5 +22,13 @@ class NightWriteTest < MiniTest::Test
     end
     expected = "0.0.0.0.0.\n00.00.0..0\n....0.0.0.\n"
     assert_equal expected, actual
+  end
+
+  def test_can_break_new_line_when_output_is_too_long
+    expected = ".00..0.0...0.0..0...000.0..000..0.000...000.0.0.0.00.00.0....0.00..00000...00..0\n" +
+    "00000.0...0.0.......0..00000.0...0.0.0....00..00....00.000..0.00000..000..00.00.\n" +
+    "0.....0.....0.........0.0.0.00000.0...........0.....0...0...0.0.0...0.....0...0.\n" +
+    ".0\n" + "00\n" + "0.\n"
+    assert_equal expected, @translate.limit_to_80_characters_writing(@long_string)
   end
 end
