0d40b6d13910ed6834ac95830514722e21060223
ninesky00 <jiangzho@gmail.com>
2020-11-08 17:17:14 -0600

    Move all to_braille methods to writing class

--- lib/night_write.rb
+++ lib/night_write.rb
@@ -1,7 +1,34 @@
+require './lib/to_english'
+
 class NightWrite
-  def write(braille_text, file_name)
-    File.open(file_name, "w") do |file|
+  include Dictionary
+  def initialize(destination)
+    @destination = destination
+  end
+
+  def write(braille_text, destination)
+    File.open(destination, "a") do |file|
       file.write(braille_text)
+    end
+  end
+
+  def convert_to_write(string)
+    output = translate_to_braille(string)
+    line1 = output.transpose[0].join
+    line2 = output.transpose[1].join
+    line3 = output.transpose[2].join
+    writing_string = line1 + "\n" + line2 + "\n" + line3 + "\n"
+  end
+
+  def limit_to_80_characters_writing(string)
+    if string.length <= 40
+      output = convert_to_write(string)
+      write(output, @destination)
+    else 
+      first40 = string.slice(0..39)
+      rest = string.slice(40..-1)
+      limit_to_80_characters_writing(first40)
+      limit_to_80_characters_writing(rest)
     end
   end
 end
