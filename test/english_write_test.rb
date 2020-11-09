4572c011737a13523dd07c54fac0362892e84a20
ninesky00 <jiangzho@gmail.com>
2020-11-08 19:10:19 -0600

    Add test to translate multiple letters to english

--- test/english_write_test.rb
+++ test/english_write_test.rb
@@ -1,20 +1,27 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/english_write'
+require './lib/night_read'
 
 class EnglishWriteTest < MiniTest::Test
   def setup
     @file1 = "./sample_text/sample_english.txt"
+    @file2 = "./sample_text/sample_braille.txt"
     @english_write = EnglishWrite.new(@file1)
-    @braille_text = "0.0.0.0.0.\n00.00.0..0\n....0.0.0.\n"
+    @reader = NightRead.new
+    @braille_text = @reader.read(@file2)
   end
 
   def test_attributes_are_instantiated
     assert_instance_of EnglishWrite, @english_write
   end
 
-  def test_can_translate_braille_to_english
-    assert_equal "hello", @english_write.translate_to_english(@braille_text)
+  def test_can_translate_one_braille_to_english
+    @braille_text = "0.\n00\n..\n"
+    assert_equal "h", @english_write.translate_to_english(@braille_text)
   end
 
+  def test_can_translate_braille_string_to_english
+    assert_equal "hello", @english_write.translate_to_english(@braille_text)
+  end
 end
