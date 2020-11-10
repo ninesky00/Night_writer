0b20b5c840ef69aa98f3ef62bc4b40743c4310b0
ninesky00 <jiangzho@gmail.com>
2020-11-10 00:23:51 -0600

    Add test case for number string

--- test/night_write_test.rb
+++ test/night_write_test.rb
@@ -4,19 +4,20 @@ require './lib/night_write'
 
 class NightWriteTest < MiniTest::Test
   def setup
-    @output_file1 = "./sample_text/test_file1.txt"
-    @output_file2 = "./sample_text/test_file2.txt"
+    @output_file1 = "./sample_text/output_file1.txt"
+    @output_file2 = "./sample_text/output_file2.txt"
+    @output_file3 = "./sample_text/output_file3.txt"
     @night_write1 = NightWrite.new(@output_file1)
     @night_write2 = NightWrite.new(@output_file2)
+    @night_write3 = NightWrite.new(@output_file3)
+    #added multiple classes for the purpose of using file apend instead of file write,
+    #need some more time to figure out how to code them interchangeably
     @braille_text = "0.0.0.0.0.\n00.00.0..0\n....0.0.0.\n"
-    @string_text = "hello"
-    @long_string = "this is a forty-one character string test"
   end
 
   def test_attributes_are_instantiated
     assert_instance_of NightWrite, @night_write1
   end
-
 
   def test_can_write_to_file
     @night_write1.write(@braille_text, @output_file1)
@@ -28,24 +29,26 @@ class NightWriteTest < MiniTest::Test
   end
 
   def test_can_translate_english_to_braille
-    expected = ["0.", "00", ".."], ["0.", ".0", ".."], ["0.", "0.", "0."], ["0.", "0.", "0."], ["0.", ".0", "0."]
-    assert_equal expected, @night_write1.translate_to_braille(@string_text)
+    string_text = "hello"
+    expected = %w(0. 00 ..), %w(0. .0 ..), %w(0. 0. 0.), %w(0. 0. 0.), %w(0. .0 0.)
+    assert_equal expected, @night_write1.translate_to_braille(string_text)
   end
 
   def test_can_convert_to_write_braille
+    string_text = "hello"
     expected = "0.0.0.0.0.\n00.00.0..0\n....0.0.0.\n"
-    assert_equal expected, @night_write1.convert_to_write(@string_text)
-    
+    assert_equal expected, @night_write1.convert_to_write(string_text)
   end
 
   def test_can_convert_capital_letters_to_write_braille
-    @string_text = "Hello"
+    caps_string = "Hello"
     expected = "..0.0.0.0.0.\n..00.00.0..0\n.0....0.0.0.\n"
-    assert_equal expected, @night_write1.convert_to_write(@string_text)
+    assert_equal expected, @night_write1.convert_to_write(caps_string)
   end
 
   def test_can_break_new_line_when_output_is_too_long
-    @night_write2.formatted_writing(@long_string)
+    long_string = "this is a forty-one character string test"
+    @night_write2.formatted_writing(long_string)
     expected = ".00..0.0...0.0..0...000.0..000..0.000...000.0.0.0.00.00.0....0.00..00000...00..0\n" +
     "00000.0...0.0.......0..00000.0...0.0.0....00..00....00.000..0.00000..000..00.00.\n" +
     "0.....0.....0.........0.0.0.00000.0...........0.....0...0...0.0.0...0.....0...0.\n" +
@@ -57,13 +60,35 @@ class NightWriteTest < MiniTest::Test
   end
 
   def test_can_indicate_capital_letters
-    @string_text = "Hello"
-    expected = ["..", "..", ".0"], ["0.", "00", ".."], ["0.", ".0", ".."], ["0.", "0.", "0."], ["0.", "0.", "0."], ["0.", ".0", "0."]
-    assert_equal expected, @night_write1.translate_to_braille(@string_text)
+    caps_string = "Hello"
+    expected = %w(.. .. .0), %w(0. 00 ..), %w(0. .0 ..), %w(0. 0. 0.), %w(0. 0. 0.), %w(0. .0 0.)
+    assert_equal expected, @night_write1.translate_to_braille(caps_string)
   end
 
   def test_strings_upcase_count
-    @string_text = "Hello World"
-    assert_equal 2, @night_write1.upcase_count(@string_text)
+    caps_string = "Hello World"
+    long_string = "this is a forty-one character string test"
+    capital_string = "This is a Forty-one Character String Test"
+    assert_equal 2, @night_write1.upcase_count(caps_string)
+    assert_equal 0, @night_write1.upcase_count(long_string)
+    assert_equal 5, @night_write1.upcase_count(capital_string)
+  end
+
+  def test_can_write_capital_string
+    caps_string = "Hello World"
+    @night_write3.formatted_writing(caps_string)
+    expected = "..0.0.0.0.0......00.0.0.00\n" +
+    "..00.00.0..0....00.0000..0\n" +
+    ".0....0.0.0....0.00.0.0...\n"
+    actual = File.open(@output_file3, "r") do |file|
+      file.read()
+    end
+    assert_equal expected, actual
+  end
+
+  def test_can_translate_numbers_to_braille
+    num_string = "5523"
+    expected = %w(.0 .0 00), %w(0. .0 ..), %w(0. .0 ..), %w(0. 0. ..), %w(00 .. ..)
+    assert_equal expected, @night_write1.translate_to_braille(num_string)
   end
 end
